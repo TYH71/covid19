8e0ad75c4be9e24a89da7d5a4a70a980a768ccca
TYH71 <64251764+TYH71@users.noreply.github.com>
2021-03-21 15:56:56 +0800

    Add files via upload
    Added Time-Series Graph

--- app.py
+++ app.py
@@ -11,6 +11,8 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import requests as req
+from streamlit.proto.DataFrame_pb2 import DataFrame
 st.markdown('''
 <style>
     body {
@@ -33,17 +35,13 @@ title = {
     "Confirmed": 'Confirmed COVID-19 Cases', 
     "Deaths": 'Death Count caused by COVID-19', 
     "Recovered": 'Recovered Cases', 
-    "Active": 'Current Active COVID-19 Cases', 
-    "Incident_Rate": 'Incident Rate', 
-    "Mortality_Rate": 'Mortality Rate'
+    "Active": 'Current Active COVID-19 Cases'
 }
 description = {
     "Confirmed": 'Counts include confirmed and probable (where reported).', 
     "Deaths": 'Counts include confirmed and probable (where reported).', 
     "Recovered": 'Recovered cases are estimates based on local media reports, and state and local reporting when available, and therefore may be substantially lower than the true number.', 
-    "Active": 'Active cases = total cases - total recovered - total deaths.', 
-    "Incident_Rate": 'Incidence Rate = cases per 100,000 persons.', 
-    "Mortality_Rate": 'Case-Fatality Ratio (%) = Number recorded deaths / Number cases.'
+    "Active": 'Active cases = total cases - total recovered - total deaths.'
 }
 
 
@@ -51,7 +49,7 @@ description = {
 st.sidebar.title('COVID-19 Dashboard')
 sidebar = st.sidebar.selectbox(
     "Feature Selection",
-    ("Confirmed", "Deaths", "Recovered", "Active", "Incident_Rate", "Mortality_Rate")
+    ("Active", "Confirmed", "Deaths", "Recovered")
 )
 st.sidebar.header('About')
 st.sidebar.info('''
@@ -68,17 +66,20 @@ with st.beta_container():
         This web application aims to deliver interative dashboard with relevant visualisations based on real time COVID-19 Pandemic Data.
         It was first identified in December 2019 in Wuhan, China. 
         The World Health Organization declared the outbreak a Public Health Emergency of International Concern in January 2020 and a pandemic in March 2020.
+        Data is sourced from John Hopkins University CSSE.
     ''')
     st.markdown('''
         > - Author :\tTYH71
         > - Data :\tCOVID-19 Data Repository by the Center for Systems Science and Engineering (CSSE) at Johns Hopkins University
         > - Source: https://github.com/CSSEGISandData/COVID-19/
+        > - Time-series API: https://covid19api.com
     ''')
     dependency = st.beta_expander('Dependencies')
     dependency.code('''
     import pandas as pd
     import plotly.express as px
     import streamlit as st
+    import requests as req
     ''')
 
 st.write('---')
@@ -98,7 +99,7 @@ with st.beta_container():
 st.write('---')
 
 
-# Bubble Map - Total {sidebar} Cases across the world
+# Bubble Map - Total COVID-19 Cases across the world
 with st.beta_container():
     st.write(f"### {title[sidebar]}")
     st.code(f'''
@@ -134,7 +135,76 @@ with st.beta_container():
 st.write('---')
 
 
-# Bar Chart - According to {sidebar} across the world
+# Time-Series Graph
+with st.beta_container():
+    st.write('### Time Series Graph')
+    st.code(f'''
+    # Time Series Graph presents the COVID-19 Data Points at each time intervals.
+    # Description: {description[sidebar]}
+    # Trivariate: Country, Date, {sidebar}
+    ''')
+    # Request List of Countries
+    countries_raw = req.get('https://api.covid19api.com/countries').json()
+    countries = pd.DataFrame.from_dict(countries_raw).sort_values(by='Country')
+    countries.reset_index(drop=True, inplace=True)
+    select_country = st.selectbox('Select Country', countries['Country'], index=200)
+    select_slug = countries[countries['Country']==select_country]['Slug'].values[0]
+    try:
+        # Request Time Series
+        time_series_raw = req.get(f"https://api.covid19api.com/live/country/{select_slug}").json()
+        time_df = pd.DataFrame.from_dict(time_series_raw)
+        if (time_df.empty):
+            raise Exception 
+        time_df['Date'] = pd.to_datetime(time_df['Date'])
+        # Visualisation
+        time_series_chart = px.line(
+            time_df,
+            x='Date', 
+            y=sidebar,
+            hover_data=['Country'],
+            title=select_country
+        )
+        time_series_chart.update_layout(
+            height=450, 
+            margin={"r":5,"t":50,"l":5,"b":50},
+            paper_bgcolor='white',
+            xaxis=dict(
+                rangeselector=dict(
+                    buttons=list([
+                        dict(count=14,
+                            label="2W",
+                            step="day",
+                            stepmode="backward"),
+                        dict(count=1,
+                            label="1M",
+                            step="month",
+                            stepmode="backward"),
+                        dict(label='All',
+                            step="all")
+                    ])
+                ),
+                rangeslider=dict(
+                    visible=True
+                ),
+                type="date"
+            ),
+            title={
+                'x':0.5,
+                'xanchor': 'center'
+            }
+        )
+        st.plotly_chart(time_series_chart)
+    except Exception as e:
+        st.code('''
+    # Time-Series Data not available.
+        ''')
+        st.write(e)
+
+
+st.write('---')
+
+
+# Bar Chart - According to Confirmed Cases across the world
 with st.beta_container():
     st.write(f"### Top 10 Countries by '{sidebar}'")
     st.code(f'''
